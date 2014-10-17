9d708cf66f94b7b186f783f9f7c4e457533479dd
Mike Casey [DartCS] <mcasey@dartmouth.edu>
2014-10-17 08:03:27 -0400

    adding demo driver

---
+++ cseparate_demo.m
@@ -0,0 +1,16 @@
+[x,sr,fmt] = wavread('gmin.wav');
+M = 7 % Number of components to separate
+fprintf(1,['separating ', num2str(M), ' components from mixture...']);
+[xhat, xhat_all] = cseparate(x, M);
+fprintf(1,'done.\n');
+fprintf(1,'hit any key to hear reconstructed mixture...\n');
+pause()
+soundsc(xhat(k,:),sr)
+for k = 1:M
+  fprintf(1,['hit any key to hear component',num2str(k),'...\n']);
+  pause()
+  soundsc(xhat(k,:),sr);
+end
+
+
+
