9589e2f6415ac5b54628110d6e9009858f183b15
Mike Casey [DartCS] <mcasey@dartmouth.edu>
2014-10-19 04:03:30 -0400

    phase reconstruction lamda function

--- cseparate.py
+++ cseparate.py
@@ -24,6 +24,8 @@ def cseparate(x, M=None, N=4096, H =1024, W=4096, max_iter=200):
     """
     M = 20 if M is None else M
 
+    phs_rec = lambda rp,dp: (np.angle(rp)+np.tile(np.atleast_2d(dp).T,rp.shape[1])).cumsum(1)
+
     F = LinearFrequencySpectrum(x, nfft=N, wfft=W, nhop=H)
     U = F._phase_map()    
     X = np.absolute(F.STFT) * np.exp(1j * np.array(F.dPhi)) # Relative phase STFT
@@ -31,14 +33,13 @@ def cseparate(x, M=None, N=4096, H =1024, W=4096, max_iter=200):
     A,S = cjade(X.T, M, max_iter) # complex-domain JADE by J. F. Cardoso
 
     AS = np.array(A*S).T # Non hermitian transpose avoids complex conjugation
-
-    Phi_hat = (np.angle(AS) + np.tile(np.atleast_2d(F.dphi).T, AS.shape[1])).cumsum(1)
+    Phi_hat = phs_rec(AS, F.dphi)
     x_hat_all = F.inverse(X_hat=np.absolute(AS), Phi_hat=Phi_hat, usewin=True)
     
     x_hat = []
     for k in np.arange(M):
         AS = np.array(A[:,k]*S[k,:]).T
-        Phi_hat = (np.angle(AS) + np.tile(np.atleast_2d(F.dphi).T, AS.shape[1])).cumsum(1)
+        Phi_hat = phs_rec(AS, F.dphi)
         x_hat.append(F.inverse(X_hat=np.absolute(AS), Phi_hat=Phi_hat, usewin=True))
 
     return x_hat, x_hat_all
